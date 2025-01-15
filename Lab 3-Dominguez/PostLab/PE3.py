77d0cccb862f94ae3deae33b2507de46b7e35fc2
J00SII <164496659+J00SII@users.noreply.github.com>
2025-01-16 00:56:31 +0800

    Update PE3.py

--- Lab 3-Dominguez/PostLab/PE3.py
+++ Lab 3-Dominguez/PostLab/PE3.py
@@ -1,3 +1,12 @@
+"""
+  LAB 03
+  PROGRAMMING PROBLEM 03
+
+  The str method of the Bank class returns a string containing the accounts in random order. Design and
+  implement a change that causes the accounts to be placed in the string by order of name. (Hint: You will
+  also have to define some methods in the SavingsAccount class.)
+"""
+
 class SavingsAccount:
     def __init__(self, name: str, balance: float):
         self.name = name
