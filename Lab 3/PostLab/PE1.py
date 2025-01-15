12039d93291dc818a891e65f2b5a1f79fe4c66d8
UWCHI <kramichooks@gmail.com>
2025-01-16 00:37:00 +0800

    Update PE1.py

--- Lab 3/PostLab/PE1.py
+++ Lab 3/PostLab/PE1.py
@@ -1,1 +1,8 @@
+"""
+  LAB 03
+  PROGRAMMING PROBLEM 01
 
+  Add methods to the Student class that compare two Student objects. One method should test for equality.
+  The other methods should support the other possible comparisons. In each case, the method returns the
+  result of the comparison of the two students’ names.
+"""
