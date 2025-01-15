e6df5821eca0dd5398f8eb77e99b0bd809c2773d
J00SII <164496659+J00SII@users.noreply.github.com>
2025-01-16 00:55:58 +0800

    Update PE1.py

--- Lab 3-Dominguez/PostLab/PE1.py
+++ Lab 3-Dominguez/PostLab/PE1.py
@@ -1,3 +1,13 @@
+"""
+  LAB 03
+  PROGRAMMING PROBLEM 01
+
+  Add methods to the Student class that compare two Student objects. One method should test for equality.
+  The other methods should support the other possible comparisons. In each case, the method returns the
+  result of the comparison of the two students’ names.
+"""
+
+
 def __eq__(self, other):
         return self.name == other.name
 class Student:
