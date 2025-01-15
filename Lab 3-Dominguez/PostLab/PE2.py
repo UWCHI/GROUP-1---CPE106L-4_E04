32e5adf55988e7702179be49e2a4c6b15665272b
J00SII <164496659+J00SII@users.noreply.github.com>
2025-01-16 00:56:16 +0800

    Update PE2.py

--- Lab 3-Dominguez/PostLab/PE2.py
+++ Lab 3-Dominguez/PostLab/PE2.py
@@ -1,3 +1,12 @@
+"""
+  LAB 03
+  PROGRAMMING PROBLEM 02
+
+  This project assumes that you have completed Project 1. Place several Student objects into a list and shuffle
+  it. Then run the sort method with this list and display all of the students’ information.
+"""
+
+
 import random
 
 class Student:
