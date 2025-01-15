2241072bda98bef68471b8a97f87e271a82817b5
JerseyEstrella <161685052+JerseyEstrella@users.noreply.github.com>
2025-01-16 00:59:02 +0800

    Update PE3.py

--- Lab 3/PostLab/PE3.py
+++ Lab 3/PostLab/PE3.py
@@ -6,6 +6,7 @@
   implement a change that causes the accounts to be placed in the string by order of name. (Hint: You will
   also have to define some methods in the SavingsAccount class.)
 """
+
 class SavingsAccount:
     def __init__(self, name: str, balance: float):
         self.name = name
