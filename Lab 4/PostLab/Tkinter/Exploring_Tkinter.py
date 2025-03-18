f0da8e12c56fb6a0ffffac128b96ef42a9d35175
UWCHI <kramichooks@gmail.com>
2025-03-18 15:25:35 +0800

    Create Exploring_Tkinter.py

---
+++ Lab 4/PostLab/Tkinter/Exploring_Tkinter.py
@@ -0,0 +1,8 @@
+import tkinter.filedialog as fd
+
+target = fd.askopenfilename()
+
+if target:                            # Check if a file was selected
+    with open(target, 'r') as file:   # Open the selected file in read mode
+        for line in file:             # Read and print each line from the file
+            print(line, end='')       # Print lines as they are without extra new lines
