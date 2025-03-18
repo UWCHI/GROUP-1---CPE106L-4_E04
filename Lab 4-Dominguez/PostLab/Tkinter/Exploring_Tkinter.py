0f9e325824f8da372b7c167930b54ec392718307
J00SII <164496659+J00SII@users.noreply.github.com>
2025-03-19 00:04:55 +0800

    Update Exploring_Tkinter.py

--- Lab 4-Dominguez/PostLab/Tkinter/Exploring_Tkinter.py
+++ Lab 4-Dominguez/PostLab/Tkinter/Exploring_Tkinter.py
@@ -1,1 +1,17 @@
 
+"""
+  LAB 04
+  PROGRAMMING PROBLEM 02
+
+  Explore the Tkinter.filedialog module to get the name of a text file
+"""
+
+import tkinter.filedialog as fd
+
+target = fd.askopenfilename()
+
+if target:                            # Check if a file was selected
+    with open(target, 'r') as file:   # Open the selected file in read mode
+        for line in file:             # Read and print each line from the file
+            print(line, end='')       # Print lines as they are without extra new lines
+
